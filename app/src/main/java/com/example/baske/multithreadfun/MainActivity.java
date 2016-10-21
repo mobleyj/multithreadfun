cea66d6f261a2716e8ff0ad7fb52e4baf29f831a
Jordan <mob08001@byui.edu>
2016-10-21 15:29:16 -0600

    Finished the entire Create case

--- app/src/main/java/com/example/baske/multithreadfun/MainActivity.java
+++ app/src/main/java/com/example/baske/multithreadfun/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.baske.multithreadfun;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.view.View.OnClickListener;
+import android.widget.ProgressBar;
+
+import java.lang.String;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button create;
-    Button load;
-    Button clear;
+    private Button create;
+    private Button load;
+    private Button clear;
+    private ProgressBar progress;
 
-    ListView numbers;
+
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         clear = (Button) findViewById(R.id._clear);
         clear.setOnClickListener(this);
 
-        numbers = (ListView) findViewById(R.id._list);
+        //list = (ListView) findViewById(R.id._list);
 
 
     }
@@ -37,6 +47,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId()) {
             case R.id._create: {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String filename = "numbers";
+                        File file = new File(getFilesDir(), filename);
+                        FileOutputStream outputStream;
+                        StringBuilder s = new StringBuilder();
+
+                        for(int i = 1; i <= 10; ++i) {
+                            s.append(i + "\n");
+                            try {
+                                Thread.sleep(250);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        try {
+                            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+
+                            outputStream.write(s.toString().getBytes());
+
+                        } catch (java.io.IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                })
+                        .start();
 
             }
 
